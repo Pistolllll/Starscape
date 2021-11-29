3b4184e5c9e0fe5b7e95e0924a5d8f75e47000df
Stagnant <82290573+ghost-2e@users.noreply.github.com>
2021-11-29 22:04:11 +0200

    Update input.lua

--- input.lua
+++ input.lua
@@ -7,34 +7,34 @@ local main_Color = ""
 local link = {"", ""}
 
 for i, v in pairs(link) do
-    link[i] = string.lower(v)
+  link[i] = string.lower(v)
 end
 
 function TableComp(t1, t2)
-    if #t1 ~= #t2 then return false end
-    local tt1 = {}
-    local tt2 = {}
-    for i, v in pairs(t1) do
-        tt1[v] = (tt1[v] or 0) + 1
-    end
-    for i, v in pairs(t2) do
-        tt2[v] = (tt2[v] or 0) + 1
-    end
-    for i, v in pairs(tt1) do
-        if tt1[i] ~= tt2[i] then return false end
-    end
-    return true
+  if #t1 ~= #t2 then return false end
+  local tt1 = {}
+  local tt2 = {}
+  for i, v in pairs(t1) do
+    tt1[v] = (tt1[v] or 0) + 1
+  end
+  for i, v in pairs(t2) do
+    tt2[v] = (tt2[v] or 0) + 1
+  end
+  for i, v in pairs(tt1) do
+    if tt1[i] ~= tt2[i] then return false end
+  end
+  return true
 end
 
 for i, v in pairs(Class) do
 	if string.lower(v) == string.lower(main_Color) then
-        local table1 = {}
+    local table1 = {}
 		if #Links[i] == #link then
 			for i2, v2 in pairs(Links[i]) do
 				table.insert(table1, string.lower(Class[v2]))
-                if TableComp(link, table1) == true then
-                    print(i)
-                end
+          if TableComp(link, table1) == true then
+            print(i)
+          end
 			end
 		end
 	end
